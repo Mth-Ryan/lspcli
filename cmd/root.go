70a4e53e190867a8e713d66644305d8803b89f8c
Mateus Ryan <mthryan@protonmail.com>
2023-11-01 17:26:55 -0300

    Runtime folder

--- cmd/root.go
+++ cmd/root.go
@@ -22,11 +22,10 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
 	"os"
+	"path"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -57,39 +56,17 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.lspcli.yaml)")
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+	runtime := path.Join(home, ".local", "share", "lspcli")
+
+	rootCmd.PersistentFlags().String("runtime", runtime, "runtime folder path")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".lspcli" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".lspcli")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
